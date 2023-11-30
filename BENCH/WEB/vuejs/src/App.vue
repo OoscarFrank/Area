08adbde313e8266e555baa7979a7c7a442a7f1ae
Oscar FRANK <oscar.frank@epitech.eu>
2023-11-30 08:57:47 +0100

    (+) login page with the redirection on home page in vue added

---
+++ BENCH/WEB/vuejs/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mainContainer">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+.mainContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+}
+</style>
