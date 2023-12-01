ca8415c9f1d991af3f86586a0190c6a38b4b7e66
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-12-01 19:46:05 +0100

    {+} add svelte login page, redirecting to main page or forgot password page

---
+++ BENCH/WEB/svelte/src/routes/HomePage.svelte
@@ -0,0 +1,18 @@
+
+  <main class="main">
+    <h1> Welcome to the home page !</h1>
+  </main>
+
+
+
+
+  <style>
+    .main {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      flex-direction: column;
+    }
+
+  </style>
