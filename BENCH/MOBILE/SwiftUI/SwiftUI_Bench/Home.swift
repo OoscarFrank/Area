5752d8706e24873497fa2f7fe7484cf2350fe4a2
Oscar FRANK <oscar.frank@epitech.eu>
2023-12-01 23:23:02 +0100

    (+) Mobile SwiftUI demoo for the bench had been added

---
+++ BENCH/MOBILE/SwiftUI/SwiftUI_Bench/Home.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct Home: View {
+
+    var body: some View {
+        ZStack {
+            Text("hello")
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ BENCH/MOBILE/SwiftUI/SwiftUI_Bench/SwiftUI_BenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BenchApp.swift
+//  SwiftUI_Bench
+//
+//  Created by Oscar Frank on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
