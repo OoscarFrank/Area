fa3c27710622445fafc12006703d0227c3124ffa
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-12-01 20:04:20 +0100

    {+} add svelte login page, redirecting to main page or forgot password page (without build folder...)

---
+++ BENCH/WEB/Svelte/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
+  import { navigate, Router, Route} from 'svelte-routing';
+  import LoginPage from './routes/LoginPage.svelte';
+  import HomePage from './routes/HomePage.svelte';
+  import ForgotPassword from './routes/ForgotPassword.svelte'
+
+  onMount(() => {
+    navigate('/');
+  });
+</script>
+
+<Router>
+  <Route path="/" component={LoginPage} />
+  <Route path="/home" component={HomePage}/>
+  <Route path="/forgotPassword" component={ForgotPassword}/>
+</Router>
---
+++ BENCH/WEB/Svelte/src/routes/ForgotPassword.svelte
@@ -0,0 +1,18 @@
+
+<main class="main">
+  <h1> OUPS !</h1>
+</main>
+
+
+
+
+<style>
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+</style>
