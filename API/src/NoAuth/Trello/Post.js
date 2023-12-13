674f999ee72140690362d7f9fe2d9ca6bdfe3d73
Oscar FRANK <oscar.frank@epitech.eu>
2023-12-13 19:56:12 +0100

    (+) important informations are displayed in the terminal when a movement is received by the Trello's webhook

--- API/src/NoAuth/Trello/Post.js
+++ API/src/NoAuth/Trello/Post.js
@@ -2,7 +2,18 @@ const utils = require("../../Utils");
 const dynamo = require("../../../DB");
 
 const postWebhook = async (req, res) => {
-    console.log(req.body);
+    const data = req.body;
+
+    const boardName = data.model.name;
+    const actionType = data.action.type;
+    const actionDate = data.action.date;
+    const memberCreatorName = data.action.memberCreator.fullName;
+
+    console.log(`Board Name: ${boardName}`);
+    console.log(`Action Type: ${actionType}`);
+    console.log(`Action Date: ${actionDate}`);
+    console.log(`Member Creator Name: ${memberCreatorName}`);
+
     res.status(201).json({ message: "Data received and processed" });
 }
 
