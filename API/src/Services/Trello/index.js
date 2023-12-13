02336f9ecd06ee0a45198f678bbbd0f392f3da09
david <david.benistant@epitech.eu>
2023-12-13 16:38:36 +0100

    [+] move trello webhook set up in noAuth

--- API/src/Services/Trello/index.js
+++ API/src/Services/Trello/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.send({ msg: "Trello" }));
 router.post("/register", require("./Register"));
-router.head("/webhook", require("./Head"));
-router.post("/webhook", require("./Post"));
+
 
 module.exports = router;
