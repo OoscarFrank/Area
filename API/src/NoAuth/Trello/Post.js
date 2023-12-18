ed648d4e3d3f5cb71d541e78f224fe1196160ef8
Oscar FRANK <oscar.frank@epitech.eu>
2023-12-18 11:20:05 +0100

    (+) first detection of an action received through the trello's webhook added (new card created)

--- API/src/NoAuth/Trello/Post.js
+++ API/src/NoAuth/Trello/Post.js
@@ -1,18 +1,51 @@
 const utils = require("../../Utils");
 const dynamo = require("../../../DB");
+const router = require("./../../Services/Router");
+
+const getUserByTrelloId = async (id) => {
+    let params = {
+        TableName: "TrelloUsers",
+        Key: {
+            id: id,
+        },
+    };
+    let tmpUser = await db.client().get(params).promise();
+    if (tmpUser.Count == 0) return null;
+
+    let user = tmpUser.Item;
+    if (!user) return null;
+    if (!user.userId) return null;
+    params = {
+        TableName: "Users",
+        Key: {
+            id: user.userId,
+        },
+    };
+    tmpUser = await db.client().get(params).promise();
+    if (tmpUser.Count == 0) return null;
+    user = tmpUser.Item;
+    return user;
+};
 
 const postWebhook = async (req, res) => {
     const data = req.body;
-
-    const boardName = data.model.name;
     const actionType = data.action.type;
-    const actionDate = data.action.date;
-    const memberCreatorName = data.action.memberCreator.fullName;
 
-    console.log(`Board Name: ${boardName}`);
-    console.log(`Action Type: ${actionType}`);
-    console.log(`Action Date: ${actionDate}`);
-    console.log(`Member Creator Name: ${memberCreatorName}`);
+    if (data.action.type == "6579fd17888c2ae55bd84e54") {
+        let user = await getUserByTrelloId(data.action.memberCreator.id);
+        if (!user) return;
+        router("trelloCreateCard", user);
+    }
+
+    // const boardName = data.model.name;
+    // const actionType = data.action.type;
+    // const actionDate = data.action.date;
+    // const memberCreatorName = data.action.memberCreator.fullName;
+
+    // console.log(`Board Name: ${boardName}`);
+    // console.log(`Action Type: ${actionType}`);
+    // console.log(`Action Date: ${actionDate}`);
+    // console.log(`Member Creator Name: ${memberCreatorName}`);
 
     res.status(201).json({ message: "Data received and processed" });
 }
