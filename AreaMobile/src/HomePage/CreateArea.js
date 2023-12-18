4261dfb18155a3b288946debb6ff71edbf37345a
cote2porc <robin.denni@epitech.eu>
2023-12-18 12:56:49 +0100

    fix(CreateArea) fix the image not showing off

--- AreaMobile/src/HomePage/CreateArea.js
+++ AreaMobile/src/HomePage/CreateArea.js
@@ -5,7 +5,7 @@ import DisplayActions from './DisplayActions';
 import DisplayReactions from './DisplayReactions';
 import * as SecureStore from 'expo-secure-store';
 import ApiRoute from '../ApiRoute/ApiRoute';
-
+import GetImages from '../GetImages/GetImages';
 
 export default function CreateArea({ showCreateArea, setShowCreateArea }) {
     const [step, setStep] = useState(0);
@@ -22,7 +22,10 @@ export default function CreateArea({ showCreateArea, setShowCreateArea }) {
                 return setCurrentScreen('login');
             }
             const data = await res.json();
-            // console.log(data);
+            let newData = Array.from(data);
+            for (let i in newData) {
+                newData[i].icon = GetImages(newData[i].app);
+            }
             setAreas(data);
         } catch (err) {
             console.log(err);
@@ -33,29 +36,6 @@ export default function CreateArea({ showCreateArea, setShowCreateArea }) {
     useEffect(() => {
         getAvailableAreas();
     }, []);
-    // const Areas = [
-    //     {
-    //         "app": "Discord",
-    //         "icon": "DiscordLogo.png",
-    //         "authUrl": "https://discord.com/api/oauth2/authorize?client_id=1183779111005597766&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FconfirmDiscord&scope=identify+email",
-    //         "actions": [
-    //             {
-    //                 "displayName": "Envoyer un message privé au bot",
-    //                 "code": "discordReceiveMp"
-    //             },
-    //             {
-    //                 "displayName": "Envoyer un message dans le serveur",
-    //                 "code": "discordReceiveServer"
-    //             }
-    //         ],
-    //         "reactions": [
-    //             {
-    //                 "displayName": "Recevoir un message privé",
-    //                 "code": "discordSendMp"
-    //             }
-    //         ]
-    //     }
-    // ]
 
     const closeModal = () => {
         setStep(0);
