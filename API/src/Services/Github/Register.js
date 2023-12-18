c518c634b6b6058b503d17fcf6c59b563970d8c9
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-12-18 10:47:59 +0100

    {+} Github authentification working

--- API/src/Services/Github/Register.js
+++ API/src/Services/Github/Register.js
@@ -9,64 +9,64 @@ const Register = async (req, res) => {
         return;
     }
 
-    fetch('https://github.com/login/oauth/access_token', {
+    let data = await fetch("https://github.com/login/oauth/access_token", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "application/json"
+            Accept: "application/json",
         },
         body: new URLSearchParams({
             client_id: process.env.GITHUB_CLIENT_ID,
             client_secret: process.env.GITHUB_CLIENT_SECRET,
             code: req.body.code,
-            redirect_uri: process.env.WEB_URL + '/confirmGithub'
-        })
-    })
-        .then(response => response.json())
-        .then(async data => {
-            if (data.access_token) {
-                // req.user.github = {
-                //     access_token: data.access_token
-                // };
-                githubUsr = {
-                    userId : req.user.id,
-                    access_token: data.access_token,
-                };
-                try {
-                    me = await request("https://api.github.com/user", githubUsr, {})
-                } catch (err) {
-                    res.status(err.status).send(err.msg)
-                    return
-                }
+            redirect_uri: process.env.WEB_URL + "/confirmGithub",
+        }),
+    });
 
-                githubUsr.id = me.id;
-                githubUsr.name = me.global_name;
-                await dynamo
-                    .client()
-                    .put({
-                        TableName: "GitHubUsers",
-                        Item: githubUsr,
-                    })
-                    .promise();
-                // if (!req.user.connected) req.user.connected = [];
-                // req.user.connected.push("github");
-                // await dynamo
-                //     .client()
-                //     .put({
-                //         TableName: "Users",
-                //         Item: req.user,
-                //     })
-                //     .promise();
-                res.status(200).send({ msg: "ok" });
-            }
-            else {
-                res.status(400).send({ msg: "Invalid code" });
-            }
-        })
-        .catch(err => {
-            console.error('Error:', err);
-        });
+    if (data.status != 200) {
+        res.status(data.status).send({ msg: "Invalid code" });
+        return;
+    }
+
+    data = await data.json();
+    if (data.access_token) {
+        console.log(data.access_token);
+        let me = null;
+        try {
+            me = await fetch("https://api.github.com/user", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${data.access_token}`,
+                },
+            });
+
+            if (me.status != 200) throw { status: 400, msg: "Invalid code" };
+            me = await me.json();
+        } catch (err) {
+            res.status(err.status).send(err.msg);
+            return;
+        }
+        if (!me) {
+            res.status(400).send({ msg: "Invalid code" });
+            return;
+        }
+        let githubUsr = {
+            userId: req.user.id,
+            access_token: data.access_token,
+            id: me.id,
+            login: me.login,
+        };
+        await dynamo
+            .client()
+            .put({
+                TableName: "GitHubUsers",
+                Item: githubUsr,
+            })
+            .promise();
+        res.status(200).send({ msg: "ok" });
+        return;
+    }
+    res.status(400).send({ msg: "Invalid code" });
 };
-
 
 module.exports = Register;
