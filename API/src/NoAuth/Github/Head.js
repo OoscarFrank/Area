7937e826bee4e9843ab6fa4c39c62d4dd22c9187
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-12-19 16:02:55 +0100

    {|} fixed utils path in github

--- API/src/NoAuth/Github/Head.js
+++ API/src/NoAuth/Github/Head.js
@@ -1,5 +1,5 @@
-const utils = require("../Utils");
-const dynamo = require("../../DB");
+const utils = require("../../Utils");
+const dynamo = require("../../../DB");
 
 const dataGithub = async (req, res) => {
     res.status(200).send();
